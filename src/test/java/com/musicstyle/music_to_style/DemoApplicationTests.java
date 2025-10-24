08be00148e89213ce9dc78ca329b29aa2996d020
Ellen Alves <milesyralves@gmail.com>
2025-10-24 20:07:57 -0300

    atualiza projeto com estrutura ajustada

---
+++ src/test/java/com/musicstyle/music_to_style/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicstyle.music_to_style;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
